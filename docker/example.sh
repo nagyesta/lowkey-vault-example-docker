b6c45fe272d6fdc671a05576b3d794d516779c9c
Esta Nagy <nagyesta@gmail.com>
2022-08-29 23:27:48 +0200

    Use custom certificate for startup (#9)
    - Updates startup arguments to use custom certificate
    - Adds new test to verify custom certificate usage
    - Disables test cache for the second run
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- docker/example.sh
+++ docker/example.sh
@@ -7,13 +7,16 @@ docker volume create -d local -o type=none -o o=bind -o device=$PWD docker-lowke
 
 # Set arguments for Lowkey Vault configuring key parameters, such as:
 # - setting the port we want to use: --server.port=8443
+# - overriding the key store type: --server.ssl.key-store-type=JKS
+# - overriding the key store path: --server.ssl.key-store=/import/cert.jks
+# - overriding the key store password: --server.ssl.key-store-password=password
 # - disable automatic vault creation: --LOWKEY_VAULT_NAMES=-
 # - turn off debug option controlling request logging: --LOWKEY_DEBUG_REQUEST_LOG=false
 # - specify the name of the import file: --LOWKEY_IMPORT_LOCATION=/import/keyvault.json.hbs
 # - defining the value of {{host}} placeholders in the import: --LOWKEY_IMPORT_TEMPLATE_HOST=localhost
 # - defining the value of {{port}} placeholders in the import: --LOWKEY_IMPORT_TEMPLATE_PORT=8443
 # More details at: https://github.com/nagyesta/lowkey-vault/tree/main/lowkey-vault-app#startup-parameters
-export LOWKEY_ARGS="--server.port=8443 --LOWKEY_VAULT_NAMES=- --LOWKEY_DEBUG_REQUEST_LOG=false --LOWKEY_IMPORT_LOCATION=/import/keyvault.json.hbs --LOWKEY_IMPORT_TEMPLATE_HOST=localhost --LOWKEY_IMPORT_TEMPLATE_PORT=8443"
+export LOWKEY_ARGS="--server.port=8443 --server.ssl.key-store-type=JKS --server.ssl.key-store=/import/cert.jks --server.ssl.key-store-password=password --LOWKEY_VAULT_NAMES=- --LOWKEY_DEBUG_REQUEST_LOG=false --LOWKEY_IMPORT_LOCATION=/import/keyvault.json.hbs --LOWKEY_IMPORT_TEMPLATE_HOST=localhost --LOWKEY_IMPORT_TEMPLATE_PORT=8443"
 
 # Start the container in a detached mode and set up port-forward, pass arguments, and mount the volume
 docker run --rm --name lowkey-docker -d -p 8443:8443 -e LOWKEY_ARGS -v docker-lowkey-vault-import:/import/:ro nagyesta/lowkey-vault:1.11.0@sha256:2262c3bbef13cfe0d1b4e89156c7206f1fe742046ee619ec32b21a242a50a57b
