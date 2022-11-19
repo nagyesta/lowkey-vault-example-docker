adb6e0e478512b399dfff3e08636f25238bec5fa
Esta Nagy <nagyesta@gmail.com>
2022-11-19 21:03:32 +0100

    Multi-arch support (#29)
    - Adds new folder for Docker compose using the multi-arch image
    - Updates Lowkey vault version to latest
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- docker/example.sh
+++ docker/example.sh
@@ -19,4 +19,4 @@ docker volume create -d local -o type=none -o o=bind -o device=$PWD docker-lowke
 export LOWKEY_ARGS="--server.port=8443 --server.ssl.key-store-type=JKS --server.ssl.key-store=/import/cert.jks --server.ssl.key-store-password=password --LOWKEY_VAULT_NAMES=- --LOWKEY_DEBUG_REQUEST_LOG=false --LOWKEY_IMPORT_LOCATION=/import/keyvault.json.hbs --LOWKEY_IMPORT_TEMPLATE_HOST=localhost --LOWKEY_IMPORT_TEMPLATE_PORT=8443"
 
 # Start the container in a detached mode and set up port-forward, pass arguments, and mount the volume
-docker run --rm --name lowkey-docker -d -p 8443:8443 -e LOWKEY_ARGS -v docker-lowkey-vault-import:/import/:ro nagyesta/lowkey-vault:1.13.23@sha256:8902c95b41abcddb42b297c176517dd46dee673f63cc6825e59f2d35b0dfa509
+docker run --rm --name lowkey-docker -d -p 8443:8443 -e LOWKEY_ARGS -v docker-lowkey-vault-import:/import/:ro nagyesta/lowkey-vault:1.13.33@sha256:9ab136a71d7fdff942fff4e824b4389461b32780392ef8a232370f54c25b8a66
