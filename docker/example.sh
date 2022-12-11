6c65d719b1be943dd97c9e34353ea9f5bdd036bf
Esta Nagy <nagyesta@gmail.com>
2022-12-11 23:02:32 +0100

    Automated version updates (#39)
    - Adds new script to automatically update image version based on docker-compose version
    - Adds new workflow to call script when image update is triggered by Renovate
    - Downgrades Lowkey Vault to be able to test change
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- docker/example.sh
+++ docker/example.sh
@@ -19,4 +19,4 @@ docker volume create -d local -o type=none -o o=bind -o device=$PWD docker-lowke
 export LOWKEY_ARGS="--server.port=8443 --server.ssl.key-store-type=JKS --server.ssl.key-store=/import/cert.jks --server.ssl.key-store-password=password --LOWKEY_VAULT_NAMES=- --LOWKEY_DEBUG_REQUEST_LOG=false --LOWKEY_IMPORT_LOCATION=/import/keyvault.json.hbs --LOWKEY_IMPORT_TEMPLATE_HOST=localhost --LOWKEY_IMPORT_TEMPLATE_PORT=8443"
 
 # Start the container in a detached mode and set up port-forward, pass arguments, and mount the volume
-docker run --rm --name lowkey-docker -d -p 8443:8443 -e LOWKEY_ARGS -v docker-lowkey-vault-import:/import/:ro nagyesta/lowkey-vault:1.14.12@sha256:9701e49f0f17d755e62f9cdbb4a137d4dab7832e6f95b7a36059c8d67829fb0e
+docker run --rm --name lowkey-docker -d -p 8443:8443 -e LOWKEY_ARGS -v docker-lowkey-vault-import:/import/:ro nagyesta/lowkey-vault:1.14.0@sha256:a0e2679172ab99121a3e6a531d3e97661315577ec613ba232b4f4aa1866adfae
---
+++ propagete-version-update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LOWKEY_VAULT_VERSION=$(grep nagyesta/lowkey-vault: < docker-compose/docker-compose.yml | cut -d ':' -f 3 | cut -d '@' -f 1)
+LOWKEY_VAULT_HASH=$(grep nagyesta/lowkey-vault: < docker-compose/docker-compose.yml | cut -d ':' -f 4)
+
+echo "Identified image version: ${LOWKEY_VAULT_VERSION}@sha256:${LOWKEY_VAULT_HASH}"
+
+# Update sh variant
+cp docker/example.sh docker/example.sh.bak
+sed "s/lowkey-vault:[0-9]\+.[0-9]\+.[0-9]\+@sha256:[0-9a-f]\+/lowkey-vault:${LOWKEY_VAULT_VERSION}@sha256:${LOWKEY_VAULT_HASH}/" < docker/example.sh.bak > docker/example.sh
+rm docker/example.sh.bak
+
+# Update multi-arch variant
+cp docker-compose-multiarch/docker-compose.yml docker-compose-multiarch/docker-compose.yml.bak
+sed "s/lowkey-vault:.\+/lowkey-vault:${LOWKEY_VAULT_VERSION}-jammy/" < docker-compose-multiarch/docker-compose.yml.bak > docker-compose-multiarch/docker-compose.yml
+rm docker-compose-multiarch/docker-compose.yml.bak
