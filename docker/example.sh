4531c513c58f76527844035203ddd52ad3111ebd
Esta Nagy <nagyesta@gmail.com>
2023-03-21 19:12:22 +0000

    Updating images {patch}
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- docker/example.sh
+++ docker/example.sh
@@ -19,4 +19,4 @@ docker volume create -d local -o type=none -o o=bind -o device=$PWD docker-lowke
 export LOWKEY_ARGS="--server.port=8443 --server.ssl.key-store-type=JKS --server.ssl.key-store=/import/cert.jks --server.ssl.key-store-password=password --LOWKEY_VAULT_NAMES=- --LOWKEY_DEBUG_REQUEST_LOG=false --LOWKEY_IMPORT_LOCATION=/import/keyvault.json.hbs --LOWKEY_IMPORT_TEMPLATE_HOST=localhost --LOWKEY_IMPORT_TEMPLATE_PORT=8443"
 
 # Start the container in a detached mode and set up port-forward, pass arguments, and mount the volume
-docker run --rm --name lowkey-docker -d -p 8443:8443 -e LOWKEY_ARGS -v docker-lowkey-vault-import:/import/:ro nagyesta/lowkey-vault:1.23.0@sha256:77e6859721333a8cd834911b52091cb8c7c6a0127fd7a951a611eea6343f4430
+docker run --rm --name lowkey-docker -d -p 8443:8443 -e LOWKEY_ARGS -v docker-lowkey-vault-import:/import/:ro nagyesta/lowkey-vault:1.24.0@sha256:78a8d1b1c8ce4a28e00911e51b88ccd28cb5c522a743cefe8587339367f6facb
