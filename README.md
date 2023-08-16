ea51d3fd142c4e3a0d56e2fd4825ae7dfafae1b9
Esta Nagy <nagyesta@gmail.com>
2023-08-16 07:24:10 +0200

    Update examples to demonstrate external configuration as well (#106)
    - Adds new external configuration files to each example
    - Splits input files into config and import folders (and volumes)
    - Updates workflows to run parallel
    - Updates readme
    - Fixes some copy-paste issues
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- README.md
+++ README.md
@@ -12,7 +12,14 @@ while using most of the features it provides.
 ### Points of interest
 
 * [docker/example.sh](docker/example.sh) shows how you can start up your container attaching the
-  [keyvault.json.hbs](docker/keyvault.json.hbs) backup file from the same folder. 
+  [keyvault.json.hbs](docker/import/keyvault.json.hbs) backup file from the import sub-folder as well as using an
+  external configuration file named [application.properties](docker/config/application.properties) from the config
+  sub-folder. 
 * [docker-compose/docker-compose.yml](docker-compose/docker-compose.yml) shows how to start up your container
-  using Docker compose including how you can import the [keyvault.json.hbs](docker-compose/keyvault.json.hbs) backup.
+  using Docker compose including the import of the [keyvault.json.hbs](docker-compose/import/keyvault.json.hbs)
+  backup and use configuration overrides from [application.properties](docker-compose/config/application.properties).
+* [docker-compose-multiarch/docker-compose.yml](docker-compose-multiarch/docker-compose.yml) shows how you need to
+  change the Docker compose setup to use a multiarch image and achieve the same outcome, i.e. import the 
+  [keyvault.json.hbs](docker-compose-multiarch/import/keyvault.json.hbs) backup and load the configuration from
+  [application.properties](docker-compose-multiarch/config/application.properties).
 
--- docker/example.sh
+++ docker/example.sh
@@ -1,14 +1,15 @@
 #!/bin/sh -l
 
-# Use docker-compose as the working directory while using this file!
+# Use docker as the working directory while using this file!
 
-# Create a volume, allowing us to share the backup file with the container by mounting it
-docker volume create -d local -o type=none -o o=bind -o device=$PWD docker-lowkey-vault-import
+# Create volumes, allowing us to share the backup file and configuration files with the container by mounting it
+docker volume create -d local -o type=none -o o=bind -o device=$PWD/import docker-lv-import
+docker volume create -d local -o type=none -o o=bind -o device=$PWD/config docker-lv-config
 
 # Set arguments for Lowkey Vault configuring key parameters, such as:
 # - setting the port we want to use: --server.port=8443
 # - overriding the key store type: --server.ssl.key-store-type=JKS
-# - overriding the key store path: --server.ssl.key-store=/import/cert.jks
+# - overriding the key store path: --server.ssl.key-store=/config/cert.jks
 # - overriding the key store password: --server.ssl.key-store-password=password
 # - disable automatic vault creation: --LOWKEY_VAULT_NAMES=-
 # - turn off debug option controlling request logging: --LOWKEY_DEBUG_REQUEST_LOG=false
@@ -16,7 +17,7 @@ docker volume create -d local -o type=none -o o=bind -o device=$PWD docker-lowke
 # - defining the value of {{host}} placeholders in the import: --LOWKEY_IMPORT_TEMPLATE_HOST=localhost
 # - defining the value of {{port}} placeholders in the import: --LOWKEY_IMPORT_TEMPLATE_PORT=8443
 # More details at: https://github.com/nagyesta/lowkey-vault/tree/main/lowkey-vault-app#startup-parameters
-export LOWKEY_ARGS="--server.port=8443 --server.ssl.key-store-type=JKS --server.ssl.key-store=/import/cert.jks --server.ssl.key-store-password=password --LOWKEY_VAULT_NAMES=- --LOWKEY_DEBUG_REQUEST_LOG=false --LOWKEY_IMPORT_LOCATION=/import/keyvault.json.hbs --LOWKEY_IMPORT_TEMPLATE_HOST=localhost --LOWKEY_IMPORT_TEMPLATE_PORT=8443"
+export LOWKEY_ARGS="--server.port=8443 --server.ssl.key-store-type=JKS --server.ssl.key-store=/config/cert.jks --server.ssl.key-store-password=password --LOWKEY_VAULT_NAMES=- --LOWKEY_DEBUG_REQUEST_LOG=false --LOWKEY_IMPORT_LOCATION=/import/keyvault.json.hbs --LOWKEY_IMPORT_TEMPLATE_HOST=localhost --LOWKEY_IMPORT_TEMPLATE_PORT=8443"
 
 # Start the container in a detached mode and set up port-forward, pass arguments, and mount the volume
-docker run --rm --name lowkey-docker -d -p 8443:8443 -e LOWKEY_ARGS -v docker-lowkey-vault-import:/import/:ro nagyesta/lowkey-vault:1.29.3@sha256:fc592452502c9b02733890fab956fd1873218d16addfaa3a8789995b489bf942
+docker run --rm --name lowkey-docker -d -p 8443:8443 -e LOWKEY_ARGS -v docker-lv-import:/import/:rw -v docker-lv-config:/config/:ro nagyesta/lowkey-vault:1.29.3@sha256:fc592452502c9b02733890fab956fd1873218d16addfaa3a8789995b489bf942
