322b86bc18397688dea44c3913a61408be3148c9
Esta Nagy <nagyesta@gmail.com>
2023-08-27 17:56:10 +0200

    Add Assumed Identity examples (#110)
    - Adds Assumed Identity to each example
    - Adds new tests to demonstrate POC with the DefaultAzureCredential
    - Updates documentation
    - Updates version bump scripts to handle both Lowkey Vault and Assumed Identity version changes using the same approach
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- README.md
+++ README.md
@@ -11,15 +11,28 @@ while using most of the features it provides.
 
 ### Points of interest
 
-* [docker/example.sh](docker/example.sh) shows how you can start up your container attaching the
-  [keyvault.json.hbs](docker/import/keyvault.json.hbs) backup file from the import sub-folder as well as using an
-  external configuration file named [application.properties](docker/config/application.properties) from the config
-  sub-folder. 
-* [docker-compose/docker-compose.yml](docker-compose/docker-compose.yml) shows how to start up your container
-  using Docker compose including the import of the [keyvault.json.hbs](docker-compose/import/keyvault.json.hbs)
-  backup and use configuration overrides from [application.properties](docker-compose/config/application.properties).
+* [docker/example.sh](docker/example.sh) shows how you can
+  * start up your Lowkey Vault container attaching the [keyvault.json.hbs](docker/import/keyvault.json.hbs)
+    backup file from the import sub-folder as well as using an external configuration file named 
+    [application.properties](docker/config/application.properties) from the config sub-folder. 
+  * start up an Assumed Identity container listening on port 8080 (only needed for the Managed Identity tests).
+* [docker-compose/docker-compose.yml](docker-compose/docker-compose.yml) shows how to 
+  * start up your container using Docker compose including the import of the 
+    [keyvault.json.hbs](docker-compose/import/keyvault.json.hbs) backup and use configuration overrides from
+    [application.properties](docker-compose/config/application.properties).
+  * start up an Assumed Identity container listening on port 8080 (only needed for the Managed Identity tests).
 * [docker-compose-multiarch/docker-compose.yml](docker-compose-multiarch/docker-compose.yml) shows how you need to
-  change the Docker compose setup to use a multiarch image and achieve the same outcome, i.e. import the 
-  [keyvault.json.hbs](docker-compose-multiarch/import/keyvault.json.hbs) backup and load the configuration from
-  [application.properties](docker-compose-multiarch/config/application.properties).
+  change the Docker compose setup to use a multiarch image and achieve the same outcome, i.e. 
+  * import the [keyvault.json.hbs](docker-compose-multiarch/import/keyvault.json.hbs) backup and load the 
+    configuration from [application.properties](docker-compose-multiarch/config/application.properties).
+  * start up an Assumed Identity container listening on port 8080 (only needed for the Managed Identity tests).
+* The two sets of tests are the following
+  * [example_test.go](test/example_test.go) contains the core set of tests using the Lowkey Vault container with a
+    `FakeCredential` for authentication.
+  * [example_managed_id_test.go](test/example_managed_id_test.go) is focusing on the changes that are necessary to
+    allow using the `DefaultAzureCredential` utilizing the fake Managed Identity tokens provided by the Assumed
+    Identity container. For these cases to work, please  don't forget to set the following environment variables:
+    * `IDENTITY_ENDPOINT` pointing to the Assumed Identity containers token endpoint. 
+      e.g., http://localhost:8080/metadata/identity/oauth2/token
+    * `IDENTITY_HEADER` using the word `header` (won't be used, just needed for an exists check)
 
---
+++ test/example_managed_id_test.go
@@ -0,0 +1,107 @@
+package test
+
+import (
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/tracing"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azcertificates"
+	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azkeys"
+	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azsecrets"
+	"testing"
+)
+
+func TestMISecret(t *testing.T) {
+	//given
+	httpClient := prepareClient()
+	cred, _ := azidentity.NewDefaultAzureCredential(nil) // Will use Managed Identity via the Assumed Identity container
+	client, _ := azsecrets.NewClient("https://localhost:8443",
+		cred,
+		&azsecrets.ClientOptions{ClientOptions: struct {
+			APIVersion       string
+			Cloud            cloud.Configuration
+			Logging          policy.LogOptions
+			Retry            policy.RetryOptions
+			Telemetry        policy.TelemetryOptions
+			TracingProvider  tracing.Provider
+			Transport        policy.Transporter
+			PerCallPolicies  []policy.Policy
+			PerRetryPolicies []policy.Policy
+		}{Transport: &httpClient}, DisableChallengeResourceVerification: true})
+
+	//when
+	value := secret(client, "secret-name")
+
+	//then
+	if value != "It worked!" {
+		t.Errorf("got %q, wanted %q", value, "It worked!")
+	}
+}
+
+func TestMIKeys(t *testing.T) {
+	//given
+	httpClient := prepareClient()
+	cred, _ := azidentity.NewDefaultAzureCredential(nil) // Will use Managed Identity via the Assumed Identity container
+	client, _ := azkeys.NewClient("https://localhost:8443",
+		cred,
+		&azkeys.ClientOptions{ClientOptions: struct {
+			APIVersion       string
+			Cloud            cloud.Configuration
+			Logging          policy.LogOptions
+			Retry            policy.RetryOptions
+			Telemetry        policy.TelemetryOptions
+			TracingProvider  tracing.Provider
+			Transport        policy.Transporter
+			PerCallPolicies  []policy.Policy
+			PerRetryPolicies []policy.Policy
+		}{Transport: &httpClient}, DisableChallengeResourceVerification: true})
+
+	//when
+	rsaKeyName := latestVersionOfKey(client, "rsa-key").KID.Name()
+	ecKeyName := latestVersionOfKey(client, "ec-key").KID.Name()
+	octKeyName := latestVersionOfKey(client, "oct-key").KID.Name()
+
+	//then
+	if rsaKeyName != "rsa-key" {
+		t.Errorf("got %q, wanted %q", rsaKeyName, "rsa-key")
+	}
+	if ecKeyName != "ec-key" {
+		t.Errorf("got %q, wanted %q", rsaKeyName, "ec-key")
+	}
+	if octKeyName != "oct-key" {
+		t.Errorf("got %q, wanted %q", rsaKeyName, "oct-key")
+	}
+}
+
+func TestMICertificates(t *testing.T) {
+	//given
+	httpClient := prepareClient()
+	cred, _ := azidentity.NewDefaultAzureCredential(nil) // Will use Managed Identity via the Assumed Identity container
+	client, _ := azcertificates.NewClient("https://localhost:8443",
+		cred,
+		&azcertificates.ClientOptions{ClientOptions: struct {
+			APIVersion       string
+			Cloud            cloud.Configuration
+			Logging          policy.LogOptions
+			Retry            policy.RetryOptions
+			Telemetry        policy.TelemetryOptions
+			TracingProvider  tracing.Provider
+			Transport        policy.Transporter
+			PerCallPolicies  []policy.Policy
+			PerRetryPolicies []policy.Policy
+		}{Transport: &httpClient}, DisableChallengeResourceVerification: true})
+
+	//when
+	rsaCert := latestVersionOfCertificate(client, "rsa-cert")
+
+	//then
+	if *rsaCert.KeyProperties.KeyType != azcertificates.JSONWebKeyTypeRSA {
+		t.Errorf("got %q, wanted %q", *rsaCert.KeyProperties.KeyType, "RSA")
+	}
+	if *rsaCert.X509CertificateProperties.Subject != "CN=example.com" {
+		t.Errorf("got %q, wanted %q", *rsaCert.X509CertificateProperties.Subject, "CN=example.com")
+	}
+	if *rsaCert.SecretProperties.ContentType != "application/x-pkcs12" {
+		t.Errorf("got %q, wanted %q", *rsaCert.SecretProperties.ContentType, "application/x-pkcs12")
+	}
+}
