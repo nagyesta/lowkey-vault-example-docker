62034f198b55da9b8b51b1210f62622f0f78b3ce
Esta Nagy <nagyesta@gmail.com>
2023-12-04 20:09:48 +0000

    Updating images {patch}
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- docker/example.sh
+++ docker/example.sh
@@ -23,4 +23,4 @@ export LOWKEY_ARGS="--server.port=8443 --server.ssl.key-store-type=JKS --server.
 docker run --rm --name lowkey-docker -d -p 8443:8443 -e LOWKEY_ARGS -v docker-lv-import:/import/:rw -v docker-lv-config:/config/:ro nagyesta/lowkey-vault:2.1.76@sha256:bd5d9bfeae4941cfb1e39604027b666adabd6218211534526aa8ad994ef8513d
 
 # If you want to rely on managed identity, then start up an Assumed Identity container on port 8080
-docker run --rm --name assumed-id-docker -d -p 8080:8080 -e ASSUMED_ID_PORT=8080 nagyesta/assumed-identity:1.1.22@sha256:329b0adeec0c1ce91c9ab8642b9c0f760258ceb5f1d1d0579ab55ffd21394694
+docker run --rm --name assumed-id-docker -d -p 8080:8080 -e ASSUMED_ID_PORT=8080 nagyesta/assumed-identity:1.1.26@sha256:a3ef771c33c93cbbe3d90d49d5d9b2a0d7d33ae23ed775c178f6ec8247c03146
