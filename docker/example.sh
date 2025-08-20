797841d6dfec83dccb26ef048ccb09c73ef697ff
Esta Nagy <nagyesta@gmail.com>
2025-08-20 16:15:48 +0000

    Updating images {patch}
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- docker/example.sh
+++ docker/example.sh
@@ -20,7 +20,7 @@ docker volume create -d local -o type=none -o o=bind -o device=$PWD/config docke
 export LOWKEY_ARGS="--server.port=8443 --server.ssl.key-store-type=JKS --server.ssl.key-store=/config/cert.jks --server.ssl.key-store-password=password --LOWKEY_VAULT_NAMES=- --LOWKEY_DEBUG_REQUEST_LOG=false --LOWKEY_IMPORT_LOCATION=/import/keyvault.json.hbs --LOWKEY_IMPORT_TEMPLATE_HOST=localhost --LOWKEY_IMPORT_TEMPLATE_PORT=8443"
 
 # Start the container in a detached mode and set up port-forward, pass arguments, and mount the volume
-docker run --rm --name lowkey-docker -d -p 8443:8443 -e LOWKEY_ARGS -v docker-lv-import:/import/:rw -v docker-lv-config:/config/:ro nagyesta/lowkey-vault:3.4.23@sha256:02a713951467903af416d76276ddda70f7f0b8b84992e199ae31da22c384a85e
+docker run --rm --name lowkey-docker -d -p 8443:8443 -e LOWKEY_ARGS -v docker-lv-import:/import/:rw -v docker-lv-config:/config/:ro nagyesta/lowkey-vault:3.4.33@sha256:3cba14bdf2ec7f9f99cd3afd823f4f659f501dd84424c59e93beeeedcb9e6c93
 
 # If you want to rely on managed identity, then start up an Assumed Identity container on port 8080
-docker run --rm --name assumed-id-docker -d -p 8080:8080 -e ASSUMED_ID_PORT=8080 nagyesta/assumed-identity:2.1.3@sha256:8f3785e3beb3c14f91707c4cc23789bcc2816bd51d1301fbcd954850491ef715
+docker run --rm --name assumed-id-docker -d -p 8080:8080 -e ASSUMED_ID_PORT=8080 nagyesta/assumed-identity:2.1.8@sha256:5f5dd7857d64e011edd4fea61f3f2895d8d0cc76ac8dc298981983843701e29d
